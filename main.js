fe51f6c73528737090f1ea337239fa595f40cdd3
Eduardo Veloso <107579577+EduardoVeloso06@users.noreply.github.com>
2024-04-25 17:31:13 -0300

    25/04/24

--- main.js
+++ main.js
@@ -14,10 +14,12 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo3 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -36,3 +38,16 @@ function calculaTempo(tempoObjetivo){
 
 
 }
+
+function atualizaCronometro(){
+    for( let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
